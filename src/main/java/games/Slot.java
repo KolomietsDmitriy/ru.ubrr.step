c0cdfe68de5a318b7d668a59d4ac3ba37933eee6
Dmitriy <kolomietsdmitriy90@gmail.com>
2021-05-11 21:28:51 +0500

    step3 openjdk11 + JitPack

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -4,4 +4,8 @@ public class Slot {
     public static void main(String... __) {
         System.out.println("Hello world!");
     }
+    
+    public String hello() {
+        return "Hello";
+    }
 }
